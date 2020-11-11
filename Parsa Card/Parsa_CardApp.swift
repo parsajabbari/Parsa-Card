c9e22c7f78f9a0a6e1069fa303ac3d3bda414854
Parsa Jabbari <parsajabbari@gmail.com>
2020-11-10 18:36:58 -0800

    Initial Commit

---
+++ Parsa Card/Parsa_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Parsa_CardApp.swift
+//  Parsa Card
+//
+//  Created by Parsa Jabbari on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Parsa_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
